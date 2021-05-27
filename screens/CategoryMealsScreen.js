223bef4b60a5d645dc451c1db9f74492c44da61e
eadogan <e.ahmetdogan@gmail.com>
2021-05-27 21:04:16 +0100

    Create Meal model and Refactor Meals Screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 
 const CategoryMealsScreen = props => {
+    const  renderMealItem = itemData => {
+        return(
+            <MealItem 
+                title={itemData.item.title}
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {}}
+            />
+        );
+    };
+
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
     return (
         <View style={styles.screen}>
-            <Text>The Category Meals Screen</Text>
-            <Button title="Go to Meal Detail" onPress={() => {
-                props.navigation.navigate('MealDetail');
-            }} /> 
+            <FlatList 
+                keyExtractor={(item, index) => item.id}
+                data={displayedMeals}
+                renderItem={renderMealItem}
+                style={{width: '90%'}}
+            />
         </View>
     );
 };
