34e78b82364faf572570dbcf86fd72d172b66543
eadogan <e.ahmetdogan@gmail.com>
2021-05-28 13:41:33 +0100

    Create MealList for general usage

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import MealList from '../components/MealList';
 
 
 const CategoryMealsScreen = props => {
-    const  renderMealItem = itemData => {
-        return(
-            <MealItem 
-                title={itemData.item.title}
-                image={itemData.item.imageUrl}
-                duration={itemData.item.duration}
-                complexity={itemData.item.complexity}
-                affordability={itemData.item.affordability}
-                onSelectMeal={() => {
-                    props.navigation.navigate('MealDetail', {mealId: itemData.item.id});
-                }}
-            />
-        );
-    };
-
     const catId = props.navigation.getParam('categoryId');
 
     const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
     return (
-        <View style={styles.screen}>
-            <FlatList 
-                keyExtractor={(item, index) => item.id}
-                data={displayedMeals}
-                renderItem={renderMealItem}
-                style={{width: '90%'}}
-            />
-        </View>
+        <MealList listData={displayedMeals} navigation={props.navigation}/>
     );
 };
 
@@ -45,13 +22,5 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
         headerTitle: selectedCategory.title
     };
 };
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
 
 export default CategoryMealsScreen;
--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { View, Text, FlatList, Button, StyleSheet, TouchableOpacity } from 'react-native';
+
+import MealList from '../components/MealList';
+import { MEALS } from '../data/dummy-data';
 
 const FavoriteScreen = props => {
+    const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
     return (
-        <Text style={styles.screen}>Fav Screen</Text>
+        <MealList listData={favMeals} navigation={props.navigation}/>
     )
 };
 
 FavoriteScreen.navigationOptions = {
     headerTitle: 'Your Favorites'
 };
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1, 
-        alignItems: 'center'
-    }
-});
 
 export default FavoriteScreen;
